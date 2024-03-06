a4619214252ea8a0b71cc66ccce8a420700c1a3f
Brian LeMaster <brian.lemaster@outlook.com>
2024-03-06 16:46:33 -0500

    added page models to MAUI project (borrowed from a previous iteration of this project)

---
+++ BasicTaskManagement.UI.MAUI/PageModels/CreateTaskGroupPageModel.cs
@@ -0,0 +1,45 @@
+﻿using BasicTaskManagement.Core.DTO;
+using BasicTaskManagement.Core.Services;
+using BasicTaskManagement.Core.Validation;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace BasicTaskManagement.UI.MAUI.PageModels;
+
+public partial class CreateTaskGroupPageModel : ObservableObject
+{
+    private readonly IDataService _dataService;
+
+    public CreateTaskGroupPageModel(IDataService dataService)
+    {
+        _dataService = dataService;
+        CreateGroup = new();
+    }
+
+    [ObservableProperty]
+    private CreateTaskGroupDTO createGroup = default!;
+
+    [RelayCommand]
+    private async Task CreateClickedAsync()
+    {
+        ValidationResult validationResult = CreateGroup.Validate();
+
+        if (!validationResult.IsValid)
+        {
+            await Shell.Current.DisplayAlert("Error!", validationResult.ErrorMessage, "OK");
+            return;
+        }
+
+        if (await _dataService.CreateTaskGroupAsync(CreateGroup) is not null)
+        {
+            await Shell.Current.Navigation.PopModalAsync();
+        }
+        else
+        {
+            await Shell.Current.DisplayAlert("Error!", "Error creating task group.", "OK");
+        }
+    }
+
+    [RelayCommand]
+    private static void CancelClicked() => Shell.Current.Navigation.PopModalAsync();
+}
---
+++ BasicTaskManagement.UI.MAUI/PageModels/TaskGroupsPageModel.cs
@@ -0,0 +1,35 @@
+﻿using BasicTaskManagement.Core.DataModels;
+using BasicTaskManagement.Core.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace BasicTaskManagement.UI.MAUI.PageModels;
+
+public partial class TaskGroupsPageModel(IDataService dataService) : ObservableObject
+{
+    private readonly IDataService _dataService = dataService;
+
+    [ObservableProperty]
+    private ReadOnlyCollection<TaskGroupData> _taskGroups = default!;
+
+    private bool IsShowComplete { get; set; }
+
+    [RelayCommand]
+    private void PageAppearing() => LoadDataAsync();
+
+    [RelayCommand]
+    private static void GroupsSelectionChanged() { }
+
+    [RelayCommand]
+    private static void CreateGroupClicked() => Shell.Current.Navigation.PushModalAsync(new CreateTaskGroupPage());
+
+    [RelayCommand]
+    private void ShowCompletedFilterChanged()
+    {
+        IsShowComplete = !IsShowComplete;
+        LoadDataAsync();
+    }
+
+    private async Task LoadDataAsync() => TaskGroups = await _dataService.GetTaskGroupsAsync(IsShowComplete);
+}
