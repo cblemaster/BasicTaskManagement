25e4cb938f6bd3d8b09db1c85db6af4c593f5cad
Brian LeMaster <brian.lemaster@outlook.com>
2024-03-06 16:16:16 -0500

    moved db conn string out of db context class and into configuration in api project

--- BasicTaskManagement.API/Context/BasicTaskManagementContext.cs
+++ BasicTaskManagement.API/Context/BasicTaskManagementContext.cs
@@ -14,9 +14,7 @@ public partial class BasicTaskManagementContext : DbContext
 
     public virtual DbSet<TaskItem> TaskItems { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.;Database=BasicTaskManagement;Trusted_Connection=true;Trust Server Certificate=true");
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
