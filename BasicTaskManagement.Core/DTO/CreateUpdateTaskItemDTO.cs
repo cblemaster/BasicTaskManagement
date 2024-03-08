1157ea2c32af55acaac19dcf40f3f2841d5474c3
Brian LeMaster <brian.lemaster@outlook.com>
2024-03-08 18:36:00 -0500

    added validation method to create update task item dto in core project

--- BasicTaskManagement.Core/DTO/CreateUpdateTaskItemDTO.cs
+++ BasicTaskManagement.Core/DTO/CreateUpdateTaskItemDTO.cs
@@ -1,4 +1,7 @@
-﻿namespace BasicTaskManagement.Core.DTO;
+﻿using BasicTaskManagement.Core.Validation;
+using System.Text;
+
+namespace BasicTaskManagement.Core.DTO;
 
 public class CreateUpdateTaskItemDTO
 {
@@ -14,4 +17,21 @@ public class CreateUpdateTaskItemDTO
     public DateTime? UpdateDate { get; set; }
     public string TaskGroupName { get; set; } = string.Empty;
     public bool TaskGroupIsFavorite { get; set; }
+
+    public ValidationResult Validate()
+    {
+        bool taskGroupIdIsValid = TaskGroupId > 0;
+        bool NameIsValid = !string.IsNullOrWhiteSpace(Name) && Name.Length > 1 && Name.Length <= 50;
+        bool NotesIsValid = Notes is null || Notes.Length <= 100;
+        bool allIsValid = taskGroupIdIsValid && NameIsValid && NotesIsValid;
+
+        if (allIsValid) { return new() { IsValid = true, ErrorMessage = string.Empty }; }
+
+        StringBuilder sb = new();
+        if (!taskGroupIdIsValid) { sb.AppendLine("Invalid task group id."); }
+        if (!NameIsValid) { sb.AppendLine("Name is required and must be 50 characters or fewer."); }
+        if (!NotesIsValid) { sb.AppendLine("If Notes are provided, they must be 100 or fewer characters."); }
+
+        return new() { IsValid = false, ErrorMessage = sb.ToString() };
+    }
 }
