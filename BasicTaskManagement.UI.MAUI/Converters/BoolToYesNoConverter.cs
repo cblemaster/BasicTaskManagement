aaea9bca496a02c4f51f647cf5869ef0ab40b6fc
Brian LeMaster <brian.lemaster@outlook.com>
2024-03-09 00:34:31 -0500

    added bool to yes/no converter in maui project

---
+++ BasicTaskManagement.UI.MAUI/Converters/BoolToYesNoConverter.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace BasicTaskManagement.UI.MAUI.Converters;
+
+public class BoolToYesNoConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is null) { return false; }
+        return (bool)value ? "Yes" : "No";
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) => throw new NotImplementedException();
+}
